7aeefc6d43993d46311d4f98f903ea386d426c8d
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-03-11 11:23:11 +0100

    claims personalizadas

--- NetCoreSeguridadEmpleados/Controllers/ManagedController.cs
+++ NetCoreSeguridadEmpleados/Controllers/ManagedController.cs
@@ -35,13 +35,24 @@ namespace NetCoreSeguridadEmpleados.Controllers
                     );
                 Claim claimName = new Claim(ClaimTypes.Name, username);
                 identity.AddClaim(claimName);
-                //COMO POR AHORA NO VOY A UTILIZAR ROLES, NO LO INDICAMOS
+
+                //podemos añadir claims personalizados o usar predefinidos para lo que queramos
+                Claim claimId = new Claim(ClaimTypes.NameIdentifier, empleado.IdEmpleado.ToString());
+                identity.AddClaim(claimId);
+                //COMO ROLE, UTILIZAMOS EL OFICIO
+                Claim claimRole = new Claim(ClaimTypes.Role, empleado.Oficio);
+                identity.AddClaim(claimRole);
+                //claim personalizado para el salario
+                Claim claimSalario = new Claim("Salario", empleado.Salario.ToString());
+                identity.AddClaim(claimSalario);
+                Claim claimDepartamento = new Claim("Departamento", empleado.IdDepartamento.ToString());
+                identity.AddClaim(claimDepartamento);
+
                 ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
                 await HttpContext.SignInAsync(
                     CookieAuthenticationDefaults.AuthenticationScheme,
                     userPrincipal
                     );
-                //POR AHORA LO ENVIAMOS A UNA LISTA QUE HAREMOS EN BREVE
                 return RedirectToAction("PerfilEmpleado", "Empleados");
             }
             else
